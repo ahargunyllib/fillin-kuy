c5b1c8a16855ba322f725d5d01ebcf1aecefe7ff
ahargunyllib <billy.bpm03@gmail.com>
2025-01-11 19:43:58 +0700

    feat: enhance Calendar component with navigation icons and code cleanup

--- src/shared/components/ui/calendar.tsx
+++ src/shared/components/ui/calendar.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { ChevronLeft, ChevronRight } from "lucide-react";
 import type * as React from "react";
 import { DayPicker } from "react-day-picker";
 
@@ -58,16 +59,14 @@ function Calendar({
 				day_hidden: "invisible",
 				...classNames,
 			}}
-			components={
-				{
-					// IconLeft: ({ className, ...props }) => (
-					// 	<ChevronLeft className={cn("h-4 w-4", className)} {...props} />
-					// ),
-					// IconRight: ({ className, ...props }) => (
-					// 	<ChevronRight className={cn("h-4 w-4", className)} {...props} />
-					// ),
-				}
-			}
+			components={{
+				IconLeft: ({ className, ...props }) => (
+					<ChevronLeft className={cn("h-4 w-4", className)} {...props} />
+				),
+				IconRight: ({ className, ...props }) => (
+					<ChevronRight className={cn("h-4 w-4", className)} {...props} />
+				),
+			}}
 			{...props}
 		/>
 	);
